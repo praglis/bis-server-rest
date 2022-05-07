864fe0897467a335754ce78fa175e751ef1c0f6d
Ida Milewska <i.milewska@gmail.com>
2022-05-07 19:11:45 +0200

    Initial commit

---
+++ src/main/java/rag/mil/bis/BisApplication.java
@@ -0,0 +1,13 @@
+package rag.mil.bis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rag/mil/bis/BisApplicationTests.java
@@ -0,0 +1,13 @@
+package rag.mil.bis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
